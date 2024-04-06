6e58e50808d4db0f07b46e3b10f7e217c90bdab4
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-06 16:43:48 +0100

    sed problems

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,34 +2,21 @@
 # a Bash script that sets up your web servers for the deployment
 
 # Install nginx if not already installed
-sudo apt-get update
-sudo apt-get -y install nginx
-
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file for testing
-echo "<html>
-<head>
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+echo "<!DOCTYPE html>
+<html>
+  <head>
   </head>
   <body>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create symbolic link if it doesn't exist
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of /data folder to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration to serve the content
-sudo sed -i '/hbnb_static/ {
-  s|^.*$|        alias /data/web_static/current/|;
-}' /etc/nginx/sites-available/default
-
-# Restart Nginx to apply changes
-sudo service nginx restart
-
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+config='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
+sed -i "38i $config" /etc/nginx/sites-available/default
+service nginx restart
 exit 0
