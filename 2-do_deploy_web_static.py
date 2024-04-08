626df2fb017f826233674a0e086f3fa4c81c6ea5
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-08 05:25:03 +0100

    sucessfully deployed my web static folder

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/bash env
+"""
+distributes an archive to your web servers
+"""
+
+from fabric.api import local
+from fabric.operations import run, put, sudo
+import os.path
+from fabric.api import env
+env.hosts = ['54.82.172.244', '100.27.14.106']
+
+
+def do_deploy(archive_path):
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        nconfig = archive_path.split("/")[-1]
+        ndir = ("/data/web_static/releases/" + nconfig.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(ndir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(nconfig, ndir))
+        run("sudo rm /tmp/{}".format(nconfig))
+        run("sudo mv {}/web_static/* {}/".format(ndir, ndir))
+        run("sudo rm -rf {}/web_static".format(ndir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(ndir))
+        return True
+    except:
+        return False
