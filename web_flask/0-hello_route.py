9ac4520bf58af796022d055def53e3f2797223cb
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-21 06:51:56 +0100

    start a flask web app and pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -18,5 +18,13 @@ def hello():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """
+    Outputs HBNB
+    """
+    return "HBNB"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
