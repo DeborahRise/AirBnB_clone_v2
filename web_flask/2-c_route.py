ea1f707cb951c7ed6568bc9d6abfea39c150f778
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-21 07:30:06 +0100

    accepts text in url

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+You must use the option strict_slashes=False in your route definition
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Outputs hello hbnb
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Outputs HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Outputs “C ” followed by value of text 
+    """
+    new_text = text.replace('_', ' ')
+    return "C " + new_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
